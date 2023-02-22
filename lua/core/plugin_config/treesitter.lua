6388d3b0754ced0a1bb4eaa2b7a93d7d569e3430
Luca Grunwald <sir_g_1@web.de>
2023-02-22 17:12:14 +0100

    set sync_install to true [treesitter]

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,7 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = { "c", "lua", "rust", "go" },
 
-  sync_install = false,
+  sync_install = true,
   auto_install = true,
   highlight = {
     enable = true,

d4832806bb10190647f6815448eb00c6629c938a
Luca Grunwald <sir_g_1@web.de>
2023-02-22 16:40:50 +0100

    initial commit

---
+++ lua/core/plugin_config/treesitter.lua
@@ -0,0 +1,10 @@
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "rust", "go" },
+
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+}
+

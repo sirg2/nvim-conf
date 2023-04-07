7b8074c09e5ed9abd64e90f1141cfebbbed5d3a9
Luca Grunwald <sir_g_1@web.de>
2023-04-07 17:59:52 +0200

    add buffer key bindings 1 to 9

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -22,3 +22,15 @@ vim.opt.scrolloff = 5
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
 vim.keymap.set('n', '<leader>c', ':nmap<CR>')
+
+
+-- buffers
+vim.keymap.set('n', '<leader>1', ':b 1<CR>')
+vim.keymap.set('n', '<leader>2', ':b 2<CR>')
+vim.keymap.set('n', '<leader>3', ':b 3<CR>')
+vim.keymap.set('n', '<leader>4', ':b 4<CR>')
+vim.keymap.set('n', '<leader>5', ':b 5<CR>')
+vim.keymap.set('n', '<leader>6', ':b 6<CR>')
+vim.keymap.set('n', '<leader>7', ':b 7<CR>')
+vim.keymap.set('n', '<leader>8', ':b 8<CR>')
+vim.keymap.set('n', '<leader>9', ':b 9<CR>')

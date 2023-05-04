e209b5dfc85f4d51646a456420cd3b1ed3bbad71
Luca Grunwald <sir_g_1@web.de>
2023-05-04 09:41:24 +0200

    Change <C-n> NvimTreeToggle

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -3,5 +3,5 @@ vim.g.loaded_netrwPlugin = 1
 
 require('nvim-tree').setup()
 
-vim.keymap.set('n', '<C-n>', ':NvimTreeFindFile<CR>')
+vim.keymap.set('n', '<C-n>', ':NvimTreeFindFileToggle<CR>')
 vim.keymap.set('n', '<C-f>', ':NvimTreeFocus<CR>')
